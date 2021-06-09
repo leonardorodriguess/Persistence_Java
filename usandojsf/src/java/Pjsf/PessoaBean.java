6a88d0f46259e3c69ef03093ae526513d0cbfcd0
Leonardo Rodrigues <leo-feitosa@live.com>
2021-06-08 21:22:59 -0300

    estudando JSF

---
+++ usandojsf/src/java/Pjsf/PessoaBean.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pjsf;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Leonardo R
+ */
+//@RequestScoped
+@ViewScoped
+@ManagedBean(name = "pessoaBean")
+
+
+
+public class PessoaBean {
+    private String nome;
+    private String sobrenome;
+    private String nomeCompleto;
+    private List<String> nomes = new ArrayList<String>();
+    
+    public String addNome(){
+        nomes.add(nome);
+        System.out.println(nomes);
+        return "";
+    }
+    
+    public String juntarNome(){
+         nomeCompleto = nome + " "+  sobrenome;
+        return "";
+    }
+
+    public List<String> getNomes() {
+        return nomes;
+    }
+
+    public void setNomes(List<String> nomes) {
+        this.nomes = nomes;
+    }
+    
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+ 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+    
+   
+    
+}
